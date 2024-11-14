f87519a9550a787c9c0c0924e4e72c8cecaf4e56
A saivenkat <110588670+saivenkatachala@users.noreply.github.com>
2024-11-14 19:42:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const validUsers = [
+    { username: '9989634265', password: '9989634265' }
+ 
+];
+
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMsg = document.getElementById('error-msg');
+
+    const user = validUsers.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        document.querySelector('.login-container').style.display = 'none';
+        document.getElementById('file-store').style.display = 'block';
+    } else {
+        errorMsg.textContent = 'Invalid username or password';
+    }
+}
 document.getElementById('search').addEventListener('input', function() {
     let filter = this.value.toUpperCase();
     let fileItems = document.querySelectorAll('.file-item');
